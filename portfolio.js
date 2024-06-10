dc15435dfb1e287387380e7dc7f8b5e94eff0a7c
Dovid Levine <dovidlevine@Dovids-MacBook-Pro.local>
2024-06-10 11:40:16 +0300

    updated highwire url

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,6 @@
 const totalHeight = document.documentElement.scrollHeight;
 
+
 document.addEventListener('mousemove', (event) => {
     const cursorLight = document.getElementById('cursor-light');
     cursorLight.style.left = `${event.clientX - cursorLight.offsetWidth / 2}px`;
@@ -13,29 +14,36 @@ document.addEventListener('mouseout', () => {
 });
 
 
-function scaleIframes() {
-    // let counter = 0;
-    //     const loop = setInterval(() => {
-    //         if(counter < 20){
-                const containers = document.querySelectorAll('.laptop-frame');
-                const iframes = document.querySelectorAll('iframe');
-                for(let i =0; i<containers.length; i++){
-                    const scaleX = containers[i].clientWidth * 0.77/ 1920;
-                    const scaleY = containers[i].clientHeight * 0.875/ 1240;
-                    const scale = Math.min(scaleX, scaleY);
-                    iframes[i].style.transform = `scale(${scale})`;
-                }
-            // } else {
-            //     clearInterval(loop);
-            // }
-            // counter++
-        // }, 51)
+function scaleLaptopIframes() {
+    const containers = document.querySelectorAll('.laptop-frame');
+    const iframes = document.querySelectorAll('.laptop-iframe');
+    for(let i =0; i<containers.length; i++){
+        const scaleX = containers[i].clientWidth * 0.77/ 1920;
+        const scaleY = containers[i].clientHeight * 0.875/ 1240;
+        const scale = Math.min(scaleX, scaleY);
+        iframes[i].style.transform = `scale(${scale})`;
+    }
 }
-// const laptopContainers = document.querySelectorAll('.laptop-outer-container', '.laptop-frame', 'iframe');
-// for (let container of laptopContainers) {
-//     container.addEventListener('mouseover', scaleIframes);
-//     container.addEventListener('mouseout', scaleIframes);
-// }
+
+function scaleIphoneIframes() {
+    const containers = document.querySelectorAll('.iphone-frame');
+    const iframes = document.querySelectorAll('.iphone-iframe');
+    for(let i =0; i<containers.length; i++){
+        const scaleX = containers[i].clientWidth * 1.99/ 852;
+        const scaleY = containers[i].clientHeight * 1.99/ 393;
+        const scale = Math.min(scaleX, scaleY);
+        iframes[i].style.transform = `scale(${scale})`;
+    }
+}
+
+function scaleIframes(){
+    console.log('window.innerWidth: ', window.innerWidth)
+    if(window.innerWidth < 900){
+        scaleIphoneIframes();
+    } else {
+        scaleLaptopIframes();
+    }
+}
 
 window.addEventListener('resize', scaleIframes);
 window.addEventListener('load', scaleIframes);
